121f59a2f985fc53e6322beb3106612d75962387
tsoarescruz <kaiphesc@gmail.com>
2017-10-04 20:14:44 -0300

    refactor(JSP): Comecando o arquivo em java

---
+++ JSP/WebApp/src/java/usuario.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 20111100428
+ */
+public class usuario {
+    private String email;
+    private String nome;
+    private String senha;
+    private String perfil;
+    private GregoryCalendar datcadastro;    
+}
