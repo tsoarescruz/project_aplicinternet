89bbb8985c41fffb4cc29f12c4e40afab84c4b6b
Thiago Cruz <tcruz@horizonte.tv.br>
2017-11-02 21:29:53 -0200

    refactor(Dao): Adicionando as classes de conexao com o banco de dados

---
+++ src/model/persistence/ClienteDao.java
@@ -0,0 +1,14 @@
+package model.persistence;
+
+public class ClienteDao {
+
+	public ClienteDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/persistence/Dao.java
@@ -0,0 +1,51 @@
+package model.persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class Dao {
+
+	// TODO Auto-generated constructor stub
+	/*
+	 * Gerenciar a conexao com o banco de dados
+	 */
+	Connection con;
+	/*
+	 * Manipular as querys no banco de dados (executar as querys)
+	 */
+	PreparedStatement stmt;
+	
+	/*
+	 * Manipular o resultado da consulta (select)
+	 */
+	ResultSet rs;
+
+	/*
+	 * URL de conexao com o banco
+	 */
+	private final String URL = "jdbc:mysql://localhost:3306/lojaonline";
+	private final String USER = "root";
+	private final String PASS = "050181";
+	
+	protected void open()throws Exception{
+		/*
+		 * Carregar o Driver de conexao com o MySQL
+		 */
+		Class.forName("com.mysql.jdbc.Driver");
+		/*
+		 * Abrir a conexao com o banco de dados
+		 */
+		con = DriverManager.getConnection(URL, USER, PASS);
+	}
+	
+	protected void close()throws Exception{
+		/*
+		 * Fechar a conexao com o banco
+		 */
+		con.close();
+	}
+
+}
